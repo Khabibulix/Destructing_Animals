747899856394af0250fc35594f76d9e07b46b2d6
Khabibulix <unitetempete@gmail.com>
2022-08-10 21:35:30 +0200

    Rebooting test

---
+++ script.js
@@ -0,0 +1,6 @@
+windows.addEventListener("load", function(){
+    const canvas = document.getElementById("game");
+    const ctx = canvas.getContext("2d");
+    canvas.width = 800;
+    canvas.height = 720;
+})
