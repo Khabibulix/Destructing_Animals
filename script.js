dc9a4d0ecfc708eb432f6947b5a1d2d06d6b1862
Khabibulix <unitetempete@gmail.com>
2022-08-10 21:58:24 +0200

    Drawing on canvas

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function(){
     class InputHandler {
         constructor(){
             this.keys = [];
-            window.addEventListener('keydown', function(e){
+            window.addEventListener('keydown', e =>{
                 if ((e.key === 'ArrowDown' || 
                     e.key === 'ArrowUp' || 
                     e.key === 'ArrowLeft' || 
@@ -13,20 +13,31 @@ window.addEventListener("load", function(){
                     && (this.keys.indexOf(e.key) === -1)){ //if key already in keys
                     this.keys.push(e.key);
                 }
-            });
+            });            
             window.addEventListener('keyup', e => {
                 if (e.key === 'ArrowDown' || 
                     e.key === 'ArrowUp' || 
                     e.key === 'ArrowLeft' || 
                     e.key === 'ArrowRight'){
                     this.keys.splice(this.keys.indexOf(e.key), 1);
-                }
+                }                
             });
         }
     }
 
     class Player {
-
+        constructor(game_width, game_height){
+            this.game_width = game_width;
+            this.game_height = game_height;
+            this.width = 200;
+            this.height = 200;
+            this.x = 0;
+            this.y = 0;
+        }
+        draw(context){
+            context.fillStyle = 'white';
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
 
     class Background {
@@ -46,6 +57,8 @@ window.addEventListener("load", function(){
     }
 
     const input = new InputHandler();
+    const player =  new Player(canvas.width, canvas.height);
+    player.draw(ctx)
 
     function animate(){
 
