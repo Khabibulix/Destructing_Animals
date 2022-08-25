c5b1592c90fd7b09b586f9677ea7b7a50c6c0cfa
Pitchumi <notyet>
2022-08-25 16:12:32 +0200

    Fixed canvas sizing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const canvas = document.getElementById("game");
-canvas.width = window.screen.width;
-canvas.height = window.screen.height - 70;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 const bricks = []
 let score = 0;
