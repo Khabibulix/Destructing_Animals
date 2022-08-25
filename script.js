c6a22ca701eece90b04d94cff3c98a9711b580c5
Khabibulix <unitetempete@gmail.com>
2022-08-25 14:40:16 +0200

    Modifying canvas dimensions via JS and modifying overflow because we don't want to see scrollbars

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const canvas = document.getElementById("game");
+canvas.width = window.screen.width;
+canvas.height = window.screen.height;
 const ctx = canvas.getContext("2d");
 const bricks = []
 let score = 0;
